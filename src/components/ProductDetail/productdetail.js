9d84140ea5de29d538cf1f6f04fb092989adbb67
Ali Kartal <ali_kartal91@hotmail.com>
2022-07-18 23:01:33 +0300

    update

--- src/components/ProductDetail/productdetail.js
+++ src/components/ProductDetail/productdetail.js
@@ -27,47 +27,31 @@ function ProductDetail() {
                             category.products.map((item, i) => (
                                 <div className="row mt-5" key={i}>
                                     <div className='col-6'>
-                                    <img 
-                                        src={item.image_url}
-                                        alt="..."
-                                        width={400}
-                                        height={400}
-                                    /> 
+                                        <img
+                                            src={item.image_url}
+                                            alt="..."
+                                            width={400}
+                                            height={400}
+                                        />
                                     </div>
 
-      
-                                <div className="col-6" >
-                                <h3  className='mb-5' style={{color:"red"}}>{item.name}</h3>
-                                <span ><AddToCart item={item} /></span>
-                                <p className='mt-3 text-danger'>{item.price.toFixed(2)} {"TL"}</p>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis officia recusandae deleniti, tempora reprehenderit voluptatem eius, nesciunt nam numquam consequuntur magni pariatur, quae provident! Atque dolores animi voluptatum eum iure.</p>
+
+                                    <div className="col-6" >
+                                        <h3 className='mb-5' style={{ color: "red" }}>{item.name}</h3>
+                                        <span ><AddToCart item={item} /></span>
+                                        <p className='mt-3 text-danger'>{item.price.toFixed(2)} {"TL"}</p>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis officia recusandae deleniti, tempora reprehenderit voluptatem eius, nesciunt nam numquam consequuntur magni pariatur, quae provident! Atque dolores animi voluptatum eum iure.</p>
+                                    </div>
+
                                 </div>
-
-                                
-                                </div>
-                                
-
-
-                                    // <div className="col-sm-6">
-                                    //     <h5 className="">{item.name}</h5>
-                                    //     <AddToCart item={item} />
-                                    //     <span className="price">
-                                    //         {item.price.toFixed(2)} {"TL"}
-                                    //     </span>
-                                    // </div>
-
-
+                            ))
+                        }
+                        <hr />
+                    </div>
                 ))
             }
-            <hr />
-        </div>
-    ))
-}
-    </div >
-)
-
-
-
+        </div >
+    )
 }
 
 export default ProductDetail;
